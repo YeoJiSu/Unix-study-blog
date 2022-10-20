dd61761d18db8656ab14ea608678c0e41aa634df
YeoJiSu <duwltn1301@naver.com>
2022-10-20 18:13:20 +0900

    Feat : Multiply two num

---
+++ 7/Test/mult.c
@@ -0,0 +1,9 @@
+/* File name: mult.c */
+#include <stdio.h>
+
+int mult(int a, int b) {
+	
+	printf("Mult function is called\n");
+	
+	return 1;
+}
---
+++ 7/Test/multi.h
@@ -0,0 +1,7 @@
+/* File name: mult.h */
+#ifndef _MULT_H_
+#define _MULT_H_
+
+int mult(int a, int b);
+
+#endif
