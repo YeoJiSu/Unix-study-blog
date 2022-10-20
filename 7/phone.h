e7b5aa439ad366c5fd5b8e493edbab1f6d78ea95
YeoJiSu <duwltn1301@naver.com>
2022-10-20 18:17:33 +0900

    Feat : Define Phonebook Information Structure

---
+++ 7/phone.h
@@ -0,0 +1,17 @@
+#ifndef MAX
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>  
+#include <string.h>
+# define MAX 50
+
+typedef struct book
+{
+    char Name[10];
+    char PhoneNumber[13];
+    struct book* link;
+} BOOK;
+
+BOOK * List;
+
+#endif
