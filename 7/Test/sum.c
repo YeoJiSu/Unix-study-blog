696b5113a7b1355fb0cd33fc598c6871c3ecbd42
YeoJiSu <duwltn1301@naver.com>
2022-10-20 18:12:33 +0900

    Feat : Add two num

---
+++ 7/Test/sum.c
@@ -0,0 +1,7 @@
+/* File name: sum.c */
+
+int sum(int a, int b) {
+	int c;
+	c = a + b;
+	return c;
+}
---
+++ 7/Test/sum.h
@@ -0,0 +1,8 @@
+/* File name: sum.h */
+
+#ifndef _SUM_H_
+#define _SUM_H_
+
+int sum(int a, int b);
+
+#endif
