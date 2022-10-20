c436c5ed3880b197f05b4c0533f1ab5c19261ce7
YeoJiSu <duwltn1301@naver.com>
2022-10-20 18:13:38 +0900

    Feat : Divide two num

---
+++ 7/Test/div.c
@@ -0,0 +1,11 @@
+/* File name: div.c */
+
+#include <stdio.h>
+
+
+int divide(int a, int b) {
+	
+	printf("Division function is called\n");
+	
+	return 1;
+}
---
+++ 7/Test/div.h
@@ -0,0 +1,8 @@
+/* File name: div.h */
+
+#ifndef _DIV_H_
+#define _DIV_H_
+
+int divide(int a, int b);
+
+#endif
